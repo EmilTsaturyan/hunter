4fc9163c44a96f097e5e76c66703b195663ae8f5
EmilTsaturyan <emiltsaturyan60@gmail.com>
2024-07-17 01:32:12 +0400

    V2

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import aiohttp
+import asyncio
+
 from sites import sites
-import requests
 
 
-def check_username_availability(username: str, sites: dict) -> dict:
+async def check_username_availability(username: str, sites: dict) -> dict:
     username_availability = {}
     for site, site_info in sites.items():
+        print(f'Checking on {site}')
         try:
             url = site_info['url'].format(username)
-            response = requests.get(url)
-            if response.status_code == 200:
-                username_availability[site] = url
-                print(f'[+] {site}: {url}')
-                continue
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status == 200:
+                        username_availability[site] = url
+                        print(f'[+] {site}: {url}')
+                        continue
         except:
             continue
 
@@ -23,12 +27,12 @@ def print_username_availability(username: str, username_availability: dict) -> N
         print(f'Username {username} is available on {site}: {url}')
 
 
-def main():
+async def main():
     username = input('Enter username: ')
     print(f'[*] Checking username {username} on:')
-    username_availability = check_username_availability(username, sites)
+    username_availability = await check_username_availability(username, sites)
     print_username_availability(username, username_availability)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
